021dbdd56c7aaa518fd361ae8dd3e6bd8bb13d47
Feiyi Wang <fwang2@gmail.com>
2013-08-28 22:44:49 -0400

    fix runtests.py to run as root, add needed ceph.conf as well

--- src/runtests.py
+++ src/runtests.py
@@ -64,7 +64,8 @@ def ORNL_sudopdsh(nodes, command):
         sudocommand += "[[ `hostname -s` = %s ]]" % node
     sudocommand += "; then %s; fi" % command
 
-    args = ['sudo', 'pdsh', '-g', 'ceph',  sudocommand]
+    #args = ['sudo', 'pdsh', '-g', 'ceph',  sudocommand]
+    args = ['pdsh', '-g', 'ceph',  sudocommand]
     print sudocommand    
     return subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
